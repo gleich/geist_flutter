2b040e32b23b59a0855c52874a0f4fc4670ee5eb
Matthew Gleich <matthewgleich@gmail.com>
2020-09-11 23:24:34 -0400

    ✨ Create basic color values for dark theme
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geist_flutter/light.dart';
+import 'package:geist_flutter/dark.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,22 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: Light().theme,
+      darkTheme: Dark().theme,
+      themeMode: ThemeMode.dark,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
